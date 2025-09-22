3942a8b7d4cbd171b445feacc45f6057265bee0f
tyPhoon <typhoonmoon1022@gmail.com>
2025-09-22 14:16:22 +0900

    feat: メッセージブロードキャスト機能の実装

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,111 @@
-use std::{env, io::Error};
+use std::{
+    collections::HashMap,
+    env,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 
-use futures_util::{future, StreamExt, TryStreamExt};
-use log::info;
+use futures_channel::mpsc::{UnboundedSender, unbounded};
+use futures_util::{StreamExt, future, stream::TryStreamExt};
+use log::{info, error, warn};
+
 use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
+    info!("WebSocket connection established: {}", addr);
+
+    let ws_stream = match tokio_tungstenite::accept_async(raw_stream).await {
+        Ok(stream) => stream,
+        Err(e) => {
+            error!("WebSocket handshake error for {}: {}", addr, e);
+            return;
+        }
+    };
+
+    // Insert the write part of this peer to the peer map.
+    let (tx, rx) = unbounded();
+    if let Ok(mut peers) = peer_map.lock() {
+        peers.insert(addr, tx);
+    } else {
+        error!("Failed to acquire peer map lock for {}", addr);
+        return;
+    }
+
+    let (outgoing, incoming) = ws_stream.split();
+
+    let broadcast_incoming = incoming.try_for_each(|msg| {
+        if let Ok(text) = msg.to_text() {
+            info!("Received message from {}: {}", addr, text);
+        }
+
+        let peers = match peer_map.lock() {
+            Ok(peers) => peers,
+            Err(_) => {
+                error!("Failed to acquire peer map lock during broadcast");
+                return future::ok(());
+            }
+        };
+
+        // We want to broadcast the message to everyone except ourselves.
+        let broadcast_recipients: Vec<_> = peers
+            .iter()
+            .filter(|(peer_addr, _)| *peer_addr != &addr)
+            .map(|(_, ws_sink)| ws_sink)
+            .collect();
+
+        for recp in broadcast_recipients {
+            if let Err(e) = recp.unbounded_send(msg.clone()) {
+                warn!("Failed to send message to peer: {}", e);
+            }
+        }
+
+        future::ok(())
+    });
+
+    let receive_from_others = rx.map(Ok).forward(outgoing);
+
+    tokio::select! {
+        result = broadcast_incoming => {
+            if let Err(e) = result {
+                error!("Error in broadcast handling for {}: {}", addr, e);
+            }
+        }
+        result = receive_from_others => {
+            if let Err(e) = result {
+                error!("Error in message forwarding for {}: {}", addr, e);
+            }
+        }
+    }
+
+    info!("{} disconnected", &addr);
+    if let Ok(mut peers) = peer_map.lock() {
+        peers.remove(&addr);
+    } else {
+        error!("Failed to acquire peer map lock during cleanup for {}", addr);
+    }
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let _ = env_logger::try_init();
-    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
+async fn main() -> Result<(), IoError> {
+    env_logger::init();
 
-    // Create the event loop and TCP listener we'll accept connections on.
-    let try_socket = TcpListener::bind(&addr).await;
-    let listener = try_socket.expect("Failed to bind");
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
+
+    let listener = TcpListener::bind(&addr).await?;
     info!("Listening on: {}", addr);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
+    while let Ok((stream, addr)) = listener.accept().await {
+        tokio::spawn(handle_connection(state.clone(), stream, addr));
     }
 
     Ok(())
-}
-
-async fn accept_connection(stream: TcpStream) {
-    let addr = stream.peer_addr().expect("connected streams should have a peer address");
-    info!("Peer address: {}", addr);
-
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
-
-    info!("New WebSocket connection: {}", addr);
-
-    let (write, read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
-        .await
-        .expect("Failed to forward messages")
 }
